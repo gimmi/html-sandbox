cbc535032127e185b083c1e496c026b9cbed4ab7
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2017-10-31 13:05:53 +0100

    angularjs-component-text

--- angularjs-component-text/app.js
+++ angularjs-component-text/app.js
@@ -9,33 +9,85 @@ angular.module('app')
             require: 'ngModel',
             link: function (scope, cmpEl, iAttrs, ngModel) {
                 var inputEl = cmpEl.find('input');
-                var changed = false;
+                var isChanged = false;
+
+                var state = 'view'; // 'focus' 'edit'
 
                 ngModel.$render = function () {
-                    inputEl.val(ngModel.$viewValue);
+                    // ngModel.$validate();
+                    if (state === 'view') {
+                        inputEl.val(formatForView(ngModel.$viewValue));
+                    } else if (state === 'focus') {
+                        inputEl.val(ngModel.$viewValue);
+                        inputEl[0].select();
+                    } else if (state === 'edit') {
+                        inputEl.val(ngModel.$viewValue);
+                        inputEl[0].select();
+                    }
                 }
 
+                var originalSetValidity = ngModel.$setValidity;
+                ngModel.$setValidity = function () {
+                    var wasValid = ngModel.$valid
+                    var ret = originalSetValidity.apply(this, arguments);
+
+                    return ret;
+                };
+
                 inputEl.on('change', function () {
-                    changed = true;
+                    if (state === 'view') {
+                        // Impossible
+                    } else if (state === 'focus') {
+                        state = 'edit';
+                    } else if (state === 'edit') {
+                        // Nothing to do
+                    }
                 })
 
+                ngModel.$parsers.push(function (value) {
+                    return value;
+                });
+
+                ngModel.$formatters.push(function (value) {
+                    return value;
+                });
+
+                ngModel.$validators.validLength = function (modelValue) {
+                    return modelValue.length < 5;
+                };
+
                 inputEl.on('blur', function () {
-                    if (changed) {
-                        if (inputEl.val().length === 5) {
-                            ngModel.$setViewValue(inputEl.val())
-                            inputEl.css({ 'direction': 'rtl' });
-                            changed = false;
-                        }
-                    } else {
+                    if (state === 'view') {
+                        // Impossible
+                    } else if (state === 'focus') {
                         inputEl.css({ 'direction': 'rtl' });
+                        inputEl.val(formatForView(ngModel.$viewValue));
+                        state = 'view'
+                    } else if (state === 'edit') {
+                        ngModel.$setViewValue(inputEl.val());
+                        // $parsers
+                        // $validators
+                        inputEl.css({ 'direction': 'rtl' });
+                        inputEl.val(formatForView(ngModel.$viewValue));
+                        state = 'view'
                     }
                 })
 
                 inputEl.on('focus', function () {
-                    inputEl.css({ 'direction': 'ltr' });
-                    // inputEl[0].select();
-                })
+                    if (state === 'view') {
+                        inputEl.css({ 'direction': 'ltr' });
+                        inputEl.val(ngModel.$viewValue);
+                        state = 'focus';
+                    } else if (state === 'focus') {
+                        // Impossible
+                    } else if (state === 'edit') {
+                        // Impossible
+                    }
+                });
 
+                function formatForView(viewValue) {
+                    return '**' + viewValue;
+                }
             }
         }
     })
