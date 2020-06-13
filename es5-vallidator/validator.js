565fd50525edfebf58f7809a5a4417221804613f
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2020-06-13 21:01:32 +0200

    es5-vallidator

--- es5-vallidator/validator.js
+++ es5-vallidator/validator.js
@@ -1,7 +1,7 @@
-Validator = function (contextObject, hierarchy) {
+Validator = function (value, hierarchy) {
     "use strict";
 
-    // if (is(contextObject, 'Null', 'Undefined')) {
+    // if (is(value, 'Null', 'Undefined')) {
     //     throw 'Invalid context object'
     // }
 
@@ -10,7 +10,7 @@ Validator = function (contextObject, hierarchy) {
     // }
 
     var _hierarchy = hierarchy,
-        _contextObject = contextObject,
+        _value = value,
         _this = Object.assign(this, {
             checkField: checkField,
             check: check,
@@ -18,7 +18,7 @@ Validator = function (contextObject, hierarchy) {
 
     function checkField(fieldName, options, nestedFn) {
 
-        new Validator(_contextObject[fieldName], _hierarchy.concat(fieldName))
+        new Validator(_value[fieldName], _hierarchy.concat(fieldName))
             .check(options, nestedFn);
 
         return _this
@@ -33,7 +33,7 @@ Validator = function (contextObject, hierarchy) {
             options.req = true;
         }
 
-        if (is(_contextObject, 'Null', 'Undefined')) {
+        if (is(_value, 'Null', 'Undefined')) {
             if (options.req) {
                 error('required but missing')
             } else {
@@ -41,15 +41,15 @@ Validator = function (contextObject, hierarchy) {
             }
         }
 
-        if (options.req && is(_contextObject, 'Null', 'Undefined')) {
+        if (options.req && is(_value, 'Null', 'Undefined')) {
             error('required but missing')
         }
 
         if (options.type === String) {
-            if (!is(_contextObject, 'String')) {
+            if (!is(_value, 'String')) {
                 error('Expected String')
             }
-            var length = _contextObject.length;
+            var length = _value.length;
             if (is(options.min, 'Number') && length < options.min) {
                 error('Expected length >= ', options.min, ' but is ', length)
             }
@@ -57,28 +57,28 @@ Validator = function (contextObject, hierarchy) {
                 error('Expected length <= ', options.max, ' but is ', length)
             }
         } else if (options.type === Number) {
-            if (!is(_contextObject, 'Number')) {
+            if (!is(_value, 'Number')) {
                 error('Expected Number')
             }
-            if (is(options.min, 'Number') && _contextObject < options.min) {
-                error('Expected >= ', options.min, ' but is ', _contextObject)
+            if (is(options.min, 'Number') && _value < options.min) {
+                error('Expected >= ', options.min, ' but is ', _value)
             }
-            if (is(options.max, 'Number') && _contextObject > options.max) {
-                error('Expected <= ', options.max, ' but is ', _contextObject)
+            if (is(options.max, 'Number') && _value > options.max) {
+                error('Expected <= ', options.max, ' but is ', _value)
             }
         } else if (options.type === Boolean) {
-            if (!is(_contextObject, 'Boolean')) {
+            if (!is(_value, 'Boolean')) {
                 error('Expected Boolean')
             }
         } else if (options.type === Object) {
-            if (!is(_contextObject, 'Object')) {
+            if (!is(_value, 'Object')) {
                 error('Expected Object')
             }
 
-            if (is(options.size, 'Number') && Object.keys(_contextObject).length !== options.size) {
-                error('Expected size ', options.size, ' but size is ', _contextObject.length)
+            if (is(options.size, 'Number') && Object.keys(_value).length !== options.size) {
+                error('Expected size ', options.size, ' but size is ', _value.length)
             }
-            var length = Object.keys(_contextObject).length;
+            var length = Object.keys(_value).length;
             if (is(options.min, 'Number') && length < options.min) {
                 error('Expected length >= ', options.min, ' but is ', length)
             }
@@ -86,11 +86,11 @@ Validator = function (contextObject, hierarchy) {
                 error('Expected length <= ', options.max, ' but is ', length)
             }
         } else if (options.type === Array) {
-            if (!is(_contextObject, 'Array')) {
+            if (!is(_value, 'Array')) {
                 error('Expected Array')
             }
 
-            var length = _contextObject.length;
+            var length = _value.length;
             if (is(options.min, 'Number') && length < options.min) {
                 error('Expected length >= ', options.min, ' but is ', length)
             }
@@ -101,19 +101,18 @@ Validator = function (contextObject, hierarchy) {
             error('Unknown type "', options.type, '"')
         }
 
-        if (is(options.enum, 'Array') && options.enum.indexOf(_contextObject) === -1) {
-            error('"', _contextObject, '" not in ', JSON.stringify(options.enum))
+        if (is(options.enum, 'Array') && options.enum.indexOf(_value) === -1) {
+            error('"', _value, '" not in ', JSON.stringify(options.enum))
         }
 
-        if (is(nestedFn, 'Function') && is(_contextObject, 'Array')) {
-            _contextObject.forEach(function (nestedVal, nestedIndex) {
+        if (is(nestedFn, 'Function') && is(_value, 'Array')) {
+            _value.forEach(function (nestedVal, nestedIndex) {
                 var nestedHierarchy = _hierarchy.concat(nestedIndex);
                 var nestedValidator = new Validator(nestedVal, nestedHierarchy);
                 nestedFn(nestedValidator, nestedVal)
             })
-        } else if (is(nestedFn, 'Function') && is(_contextObject, 'Object')) {
-            var nestedValidator = new Validator(_contextObject, _hierarchy);
-            nestedFn(nestedValidator, _contextObject)
+        } else if (is(nestedFn, 'Function') && is(_value, 'Object')) {
+            nestedFn(_this, _value)
         }
     }
 
