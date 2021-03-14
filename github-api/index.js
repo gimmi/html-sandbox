1cf3faad167ef22228818917fbff607903c94af8
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2021-03-14 12:14:05 +0100

    github-api

---
+++ github-api/index.js
@@ -0,0 +1,31 @@
+import { Octokit } from 'https://cdn.skypack.dev/@octokit/rest'
+
+const loadEl = document.getElementById('load')
+const authEl = document.getElementById('auth')
+const outputEl = document.getElementById('output')
+
+authEl.value = localStorage.getItem('auth')
+
+loadEl.addEventListener('click', async () => {
+    const auth = authEl.value
+    const octokit = new Octokit({ auth })
+
+    localStorage.setItem('auth', auth)
+
+    const { data: repo } = await octokit.request('/repos/gimmi/brain')
+
+    // Inspired by https://gist.github.com/testcollab/1236348
+    const { data: ref } = await octokit.git.getRef({
+        owner: repo.owner.login,
+        repo: repo.name,
+        ref: `heads/${repo.default_branch}`
+    });
+
+    const { data: tree } = await octokit.git.getTree({
+        owner: repo.owner.login,
+        repo: repo.name,
+        tree_sha: ref.object.sha,
+    });
+
+    outputEl.textContent = JSON.stringify(tree, null, 2)
+})
