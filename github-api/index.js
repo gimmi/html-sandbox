6911b67b6f6f78d3ea94f0f05e317a1ff4422840
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2024-12-13 18:43:51 +0100

    github-api

--- github-api/index.js
+++ github-api/index.js
@@ -10,51 +10,59 @@ authEl.value = localStorage.getItem('auth')
 
 loadEl.addEventListener('click', async () => {
     const auth = authEl.value
+    const owner = "gimmi"
+    const repo = "brain"
     const { rest: octokit } = new Octokit({ auth })
 
     localStorage.setItem('auth', auth)
 
-    const { data: repo } = await octokit.repos.get({
-        owner: 'gimmi',
-        repo: 'brain'
-    })
-
-    const { data: file } = await octokit.repos.getContent({
-        owner: repo.owner.login,
-        repo: repo.name,
-        path: '/Bookmarks.yaml'
-    })
-    
-    const content = YAML.parse(atob(file.content))
-
-    console.dir(content)
+    const { data: repoData } = await octokit.repos.get({ owner, repo })
 
     // Inspired by https://gist.github.com/testcollab/1236348
-    const { data: ref } = await octokit.git.getRef({
-        owner: repo.owner.login,
-        repo: repo.name,
-        ref: `heads/${repo.default_branch}`
+    const { data: refData } = await octokit.git.getRef({
+        owner,
+        repo,
+        ref: `heads/${repoData.default_branch}`
     });
 
-    const { data: tree } = await octokit.git.getTree({
-        owner: repo.owner.login,
-        repo: repo.name,
-        tree_sha: ref.object.sha
+    const { data: treeData } = await octokit.git.getTree({
+        owner,
+        repo,
+        tree_sha: refData.object.sha
     });
 
-    for (let it of tree.tree) {
-        listEl.textContent += `${it.path}${it.type === 'tree' ? '/' : ''} [${it.sha}]\n`
+    const files = await getFiles(owner, repo, refData.object.sha)
+    listEl.textContent = files
+        .map(x => `${x.path} [${x.sha}]`)
+        .join("\n")
 
-        if (it.path === 'Docker.md') {
-            const { data: blob } = await octokit.git.getBlob({
-                owner: repo.owner.login,
-                repo: repo.name,
-                file_sha: it.sha
-            })
+    // for (let it of treeData.tree) {
+    //     listEl.textContent += `${it.path}${it.type === 'tree' ? '/' : ''} [${it.sha}]\n`
 
-            const content = blob.encoding === 'base64' ? window.atob(blob.content) : blob.content;
-            contentEl.textContent = content;
+    //     if (it.path === 'Docker.md') {
+    //         const { data: blob } = await octokit.git.getBlob({
+    //             owner: repoData.owner.login,
+    //             repo: repoData.name,
+    //             file_sha: it.sha
+    //         })
+
+    //         const content = blob.encoding === 'base64' ? window.atob(blob.content) : blob.content;
+    //         contentEl.textContent = content;
+    //     }
+    // }
+
+    async function getFiles(owner, repo, tree_sha, prefix) {
+        const files = []
+        const { data: treeData } = await octokit.git.getTree({ owner, repo, tree_sha });
+        for (let it of treeData.tree) {
+            if (it.type === 'tree') {
+                const childFiles = await getFiles(owner, repo, it.sha)
+                files.push(...childFiles)
+            } else {
+                files.push({ path: `${prefix}/${it.path}`, sha: it.sha })
+            }
         }
-    }
 
+        return files
+    }
 })
