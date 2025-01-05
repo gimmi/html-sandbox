072ff357c25241c95feed452e342083941081156
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2025-01-05 11:52:20 +0100

    bookmarks

--- bookmarks/links.js
+++ bookmarks/links.js
@@ -1,5 +1,5 @@
 import { h } from 'preact';
 
 export default function Links({ children }) {
-    return h("ul", {}, children)
+    return h("div", { style: { paddingLeft: "1em", borderLeft: "1px dashed" }}, children)
 }
