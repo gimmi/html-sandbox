572daba52a491c274f4d112d61054e29cbe4cf7c
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2025-02-25 20:27:09 +0100

    bookmarks

--- bookmarks/link.js
+++ bookmarks/link.js
@@ -1,4 +1,5 @@
 import { h, Fragment } from 'preact';
+import _ from 'lodash';
 
 export default function Link({ link }) {
     const title = link.hlLen ?
@@ -9,9 +10,7 @@ export default function Link({ link }) {
         ] :
         link.title
 
-    const linkEl = link.href ?
-        h("a", { href: link.href, class: "secondary", style: "display: block;", target: "_blank" }, title) :
-        h("span", { style: "display: block;" }, title)
+    const linkEl = link.href ? mklink() : h("span", { style: "display: block;" }, title)
 
     const subLinks = link?.links || []
     const subLinksEl = subLinks.length ?
@@ -19,4 +18,18 @@ export default function Link({ link }) {
         null
 
     return h(Fragment, {}, [ linkEl, subLinksEl ])
+
+    function mklink() {
+        // const favicon = "https://s2.googleusercontent.com/s2/favicons?domain=" + new URL(link.href).hostname
+        const favicon = link.favicon === true ? 
+            new URL(link.href).origin + "/favicon.ico" :
+            link.favicon
+
+        return _.isString(favicon) ?
+            h("a", { href: link.href, class: "secondary", style: "display: flex; align-items: center; column-gap: 4px;", target: "_blank" }, 
+                h("img", { src: favicon, style: "width: 16px; height: 16px;" }),
+                h("span", {}, title)
+            ) :
+            h("a", { href: link.href, class: "secondary", style: "display: block;", target: "_blank" }, title)
+    }
 }
