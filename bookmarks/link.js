47f1b8eec1bd9d88371a9a63c06e4903a8b50e0f
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2025-03-22 12:40:24 +0100

    Light symbol

--- bookmarks/link.js
+++ bookmarks/link.js
@@ -7,7 +7,7 @@ export default function Link({ link, level = 0 }) {
     const favicon = mkfavicon()
     
     let els = [
-        Array.from({ length: level }).map(_ => h("div", { style: { padding: "0 .3em 0 .3em" }}, "❱"))
+        Array.from({ length: level }).map(_ => h("div", { style: { padding: "0 .3em 0 .3em" }}, "❭"))
     ]
 
     if (favicon) {
